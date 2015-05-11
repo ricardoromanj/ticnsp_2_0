7dcfca06c769f7a9eb1fcca72ec0942495aa71a8
Ricardo Roman J <ricardoromanjimenez@gmail.com>
2015-05-11 00:05:39 -0600

    Added cordinator images

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,54 @@
 class MainController < ApplicationController
   def index
+  		@coord_info = {
+  			'ana' => {
+  				name: 'Ana',
+  				position: 'Coordinadora general'
+  				},
+  			'dan' => {
+  				name: 'Daniel',
+  				position: 'Coordinador'
+  				},
+  			'hil' => {
+  				name: 'Hilda',
+  				position: 'Coordinadora'
+  				},
+  			'ils' => {
+  				name: 'Ilse',
+  				position: 'Coordinadora'
+  				},
+  			'isa' => {
+  				name: 'Isabel',
+  				position: 'Coordinadora'
+  				},
+  			'jor' => {
+  				name: 'Jorge',
+  				position: 'Coordinador'
+  				},
+  			'lau' => {
+  				name: 'Laura',
+  				position: 'Coordinadora'
+  				},
+  			'med' => {
+  				name: 'Medellín',
+  				position: 'Coordinador general'
+  				},
+  			'ola' => {
+  				name: 'Olaves',
+  				position: 'Coordinador'
+  				},
+  			'pao' => {
+  				name: 'Paola',
+  				position: 'Coordinadora'
+  			},
+  			'gab' => {
+  				name: 'Gaby',
+  				position: 'Coordinadora'
+  			},
+  			'ric' => {
+  				name: 'Ricardo',
+  				position: 'Coordinador'
+  			}
+  		}
   end
 end
